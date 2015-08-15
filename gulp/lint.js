b9021140a90e60336d69cce5e3bedfc056f99f74
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2015-08-15 20:38:42 +0000

    Swap to jshint to eslint

--- gulp/lint.js
+++ gulp/lint.js
@@ -4,12 +4,12 @@ var stylish = require('jshint-stylish');
 
 module.exports = function (gulp, plugins, sources, destinations, config) {
 
-  gulp.task('jshint', function() {
+  gulp.task('eslint', function() {
     return gulp.src( sources.scripts.glob )
-      .pipe( plugins.jshint() )
-      .pipe( plugins.jshint.reporter( stylish ) );
+      .pipe( plugins.eslint() )
+      .pipe( plugins.eslint.format() )
   });
 
-  gulp.task( 'lint:scripts', [ 'jshint' ] )
+  gulp.task( 'lint:scripts', [ 'eslint' ] )
 
 }
