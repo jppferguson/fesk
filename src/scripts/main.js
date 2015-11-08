8f8c3b89437eee62fa3b5e829913e88195e7a3a2
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2015-11-08 22:53:33 +0000

    Add some ES6 stuff into the main.js file so we can prove it works

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
 'use strict'
 
+import _ from 'lodash'
+
 // the main script file
-console.log('Oh hai there!');
+console.log( _.trim( '__ Oh hai, can i haz teh codez plz? __', '_ ' ) ) // eslint-disable-line
