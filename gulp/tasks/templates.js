b2274d0b7b1f058ecc299bc627c29b05a724bd99
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2015-12-16 00:18:52 +0000

    Remove plumber from templates

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -4,14 +4,14 @@ import browserSync from 'browser-sync'
 import config      from '../config'
 import gulp        from 'gulp'
 import jade        from 'gulp-jade'
-import plumber     from 'gulp-plumber'
+import handleError from '../helpers/handle-error'
 
 gulp.task( 'templates:build', function() {
   return gulp.src( config.sources.templates.glob )
-    .pipe( plumber() )
     .pipe( jade( {
       pretty: true
     } ) )
+    .on( 'error', handleError )
     .pipe( gulp.dest( config.destinations.root ) )
 } )
 
