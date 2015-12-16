476e5280eaaaca3a893fdd9175066dd7011c44c5
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2015-12-16 00:38:39 +0000

    Del finally has promises!

--- gulp/tasks/utilities.js
+++ gulp/tasks/utilities.js
@@ -4,6 +4,6 @@ import config from '../config'
 import del    from 'del'
 import gulp   from 'gulp'
 
-gulp.task( 'clean', function( cb ) {
-  del( [ config.destinations.root + '/**/*' ], cb )
+gulp.task( 'clean', function() {
+  return del( [ config.destinations.root + '/**/*' ] )
 } )
